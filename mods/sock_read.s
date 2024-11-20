a881628910bfcfd214364760eeaa1354f639f2f8
Zakharii-Husar <zakhariihusar@gmail.com>
2024-11-19 18:41:19 -0600

    Finished modifying error handling and return values in sock_read

--- mods/sock_read.s
+++ mods/sock_read.s
@@ -5,7 +5,11 @@ GET_STRING: .asciz "GET"
 
 sock_read_err_msg:    .asciz "\033[31mFailed to read client request! ❌\033[0m\n"
 
+bad_request_path: .asciz "./asm_server/public/400.html"
+
 not_found_path: .asciz "./asm_server/public/404.html"
+
+server_err_path: .asciz "./asm_server/public/500.html"
 
 .section .bss
 .lcomm request_buffer, 1024  # Allocates 1024 bytes for the request_buffer, zero-initialized
@@ -17,18 +21,19 @@ not_found_path: .asciz "./asm_server/public/404.html"
 # Implicit parameters:
 # - %r12 Socket file descriptor;
 # Return Values:
-#   - Returns the number of bytes read on success (>= 0)
-#   - Returns negative num (error code)
+#   - %rax: Pointer to the response_content_buffer
+#   - %rdi: Actual file size (size of the opened file)
+#   - %rdx: Error code (0 if no error, negative value for specific errors)
 # Side effects:
 #   - Extracts the route and route extension into buffers
 #   - Opens requested file into buffer
 
 .type sock_read, @function
 sock_read:
-    push %rbp                              # save the caller's base pointer
-    mov %rsp, %rbp                         # set the new base pointer (stack frame)
+    push %rbp                               # save the caller's base pointer
+    mov %rsp, %rbp                          # set the new base pointer (stack frame)
 
-    # READ CLIENT'S REQUEST
+    # READ AND VALIDATE CLIENT'S REQUEST
     mov $0, %rdx                            # Set %rdx to 0 for flags if needed
     mov %r12, %rdi                          # client socket file descriptor
     lea request_buffer(%rip), %rsi          # pointer to the request_buffer to store the request
@@ -57,35 +62,78 @@ sock_read:
     cmp $-1, %rax                            # Check if file_open returned -1 (error)
     jl file_not_found                        # Jump if file not found
 
-    lea response_content_buffer(%rip), %rax  # Return pointer to response_content_buffer
-    pop %rbp                                 # restore the caller's base pointer
-    ret    
+    mov $0, %rdx                             # Set error code to 0 (no error)
+    jmp finish_sock_read 
+
+# HANDLE ERRORS
 
 file_not_found:
-    # Clear the response_content_buffer before the second attempt
+    # Clear the response_content_buffer before the next attempt
     lea response_content_buffer(%rip), %rdi   # Load address of response_content_buffer
-    mov $response_content_buffer_size, %rsi                           # Number of bytes to clear
+    mov $response_content_buffer_size, %rsi   # Number of bytes to clear
     call clear_buffer
 
-    lea not_found_path(%rip), %rdi          # Load not_found_path as the file path
-    lea response_content_buffer(%rip), %rsi  # Use the same buffer for the response
-    call file_open                           # Attempt to open the not found file
+    lea not_found_path(%rip), %rdi            # Load not_found_path as the file path
+    lea response_content_buffer(%rip), %rsi   # Use the same buffer for the response
+    call file_open                            # Attempt to open the not found file
 
-    cmp $-1, %rax                            # Check if the second file_open returned -1 (error)
-    jl bad_request                            # Jump to bad_request if it fails
+    cmp $-1, %rax                             # Check if the second file_open returned -1 (error)
+    jl server_err                            # Jump to bad_request if it fails
 
-    lea response_content_buffer(%rip), %rax  # Return pointer to response_content_buffer
-    pop %rbp                                 # restore the caller's base pointer
-    ret    
+    mov $-1, %rdx                             # Set error code to -1 (file not found)
+    jmp finish_sock_read
 
 bad_request:
-    mov $-1, %rax
-    pop %rbp
-    ret
+    # Clear the response_content_buffer before the next attempt
+    lea response_content_buffer(%rip), %rdi   # Load address of response_content_buffer
+    mov $response_content_buffer_size, %rsi   # Number of bytes to clear
+    call clear_buffer
+
+    lea bad_request_path(%rip), %rdi          # Load not_found_path as the file path
+    lea response_content_buffer(%rip), %rsi   # Use the same buffer for the response
+    call file_open                            # Attempt to open the not found file
+
+    cmp $-1, %rax                             # Check if the second file_open returned -1 (error)
+    jl server_err                             # Jump to bad_request if it fails
+
+    mov $-2, %rdx                             # Set error code to -2 (bad request)
+    jmp finish_sock_read
 
 method_not_allowed:
-    mov $-2, %rax
-    pop %rbp
+    # Clear the response_content_buffer before the next attempt
+    lea response_content_buffer(%rip), %rdi   # Load address of response_content_buffer
+    mov $response_content_buffer_size, %rsi   # Number of bytes to clear
+    call clear_buffer
+
+    lea method_not_allowed_path(%rip), %rdi          # Load not_found_path as the file path
+    lea response_content_buffer(%rip), %rsi   # Use the same buffer for the response
+    call file_open                            # Attempt to open the not found file
+
+    cmp $-1, %rax                             # Check if the second file_open returned -1 (error)
+    jl server_err                             # Jump to bad_request if it fails
+
+    mov $-3, %rdx                             # Set error code to -3 (method not allowed)
+    jmp finish_sock_read
+
+server_err:
+    # Clear the response_content_buffer before the next attempt
+    lea response_content_buffer(%rip), %rdi   # Load address of response_content_buffer
+    mov $response_content_buffer_size, %rsi   # Number of bytes to clear
+    call clear_buffer
+
+    lea bad_request_path(%rip), %rdi          # Load not_found_path as the file path
+    lea response_content_buffer(%rip), %rsi   # Use the same buffer for the response
+    call file_open                            # Attempt to open the not found file
+
+    mov $-4, %rdx                             # Set error code to -4 (server error)
+    jmp finish_sock_read
+
+
+finish_sock_read:
+    mov %rax, %rdi                            # Set content size
+    lea response_content_buffer(%rip), %rax   # Return pointer to response_content_buffer
+    pop %rbp                                  # restore the caller's base pointer
     ret
+
 
  
