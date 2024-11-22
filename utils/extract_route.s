57452644d7a6783b81e1b0ce714328a4c1808b5b
Zakharii-Husar <zakhariihusar@gmail.com>
2024-11-22 16:04:00 -0600

    Modified extract route to preserve buffer values in r12, r13

--- utils/extract_route.s
+++ utils/extract_route.s
@@ -1,14 +1,15 @@
 # Function: extract_route
 # Input: 
-#   %rdi - pointer to the request_content_buffer
-#   %rsi - pointer to the request_route_buffer
+#   %rdi - pointer to the request_route_buffer
+#   %rsi - pointer to the request_content_buffer
 # Output:  none
 
 .section .data
 .equ SPACE, 32                    # ASCII code for space character
 index_str: .asciz "index"
+index_str_len = . - index_str
 html_ext: .asciz ".html"          # HTML extension definition moved here
-
+html_ext_len = . - html_ext
 slash_string: .asciz "/" 
 
 .section .text
@@ -18,56 +19,64 @@ slash_string: .asciz "/"
 extract_route:
     push %rbp
     mov %rsp, %rbp
+    push %r12
+    push %r13
+    
+    mov %rdi, %r12                   # Save request_route_buffer
     
     # Find first space using str_find_char
-    mov %rdi, %rsi                   # Move request buffer to %rsi for str_find_char
-    mov $SPACE, %rdi                 # Move space character to %rdi
+    mov %rsi, %rdi                   # Move request_content_buffer to rdi
+    mov $SPACE, %rsi                 # Move space character to rsi
     call str_find_char               # Find first space
-    mov %rax, %rdi                   # Move result pointer back to %rdi
-    inc %rdi                         # Skip the space
+    inc %rax                         # Skip the space
+    mov %rax, %r13                   # Save start of route in callee-saved register
 
     # Find end of route (next space)
-    mov %rdi, %r9                    # Save start of route in r9
-    mov %rdi, %rsi                   # Current position to %rsi for str_find_char
-    mov $SPACE, %rdi                 # Space character to %rdi
+    mov %r13, %rdi                   # Move request_content_buffer to rdi
+    mov $SPACE, %rsi                 # Move space character to rsi
     call str_find_char               # Find next space
-    mov %rax, %r8                    # Save end position in r8
 
-    # Calculate length and copy route
-    mov %rsi, %rdi                    # Destination is now from parameter (rsi)
-    mov %r9, %rsi                    # Start of route to source
-    mov %r8, %rdx
-    sub %r9, %rdx                    # Calculate length (end - start)
-    call str_concat                  # Copy route to buffer
 
-    # Check if route is just "/"
-    mov %rsi, %rsi                   # Route buffer already in rsi
-    lea slash_string(%rip), %rdi
-    call str_cmp
-    cmp $0, %rax
-    je search_for_extension          
 
-    # Append "index" to the route
-    mov %rsi, %rsi                   # Route buffer already in rsi
-    lea index_str(%rip), %rdi        
-    xor %rdx, %rdx                   
+    # Prepare arguments for str_concat
+    mov %r12, %rdi                   # Restore the destination buffer
+    mov %r13, %rsi                   # Restore start of route from stack
+    mov %rax, %rdx                  # End position returned by str_find_char to rdx
+    sub %r13, %rdx                  # Calculate length (end - start)
+    # Copy route to buffer
     call str_concat
 
-search_for_extension:                
-    mov %rsi, %rsi                   # Route buffer already in rsi
-    mov $'.', %rdi
-    call str_find_char
-    mov %rax, %r10                   
+
+    # Check if route is just "/"
+    mov %r12, %rdi                   # Move request_route_buffer to rdi
+    lea slash_string(%rip), %rsi
+    call str_cmp
+    cmp $0, %rax
+    jne search_for_extension         # Skip index append if not "/"
+
+    # Append "index" to the route
+    mov %r12, %rdi                   # Move request_route_buffer to rdi
+    lea index_str(%rip), %rsi        # Load "index" as source string into %rsi
+    mov $index_str_len, %rdx         # Length of "index"
+    call str_concat
+
+search_for_extension:          
+    mov %r12, %rdi                   # Move request_route_buffer to rdi
+    mov $'.', %rsi
+    call str_find_char      
     cmp $1, %rdx
     je exit_extract_route 
 
 no_extension:
     # Append .html extension
-    mov %rsi, %rsi                   # Route buffer already in rsi
-    lea html_ext(%rip), %rdi         
-    xor %rdx, %rdx                   
+    mov %r12, %rdi                   # Move request_route_buffer to rdi
+    lea html_ext(%rip), %rsi         
+    mov $html_ext_len, %rdx          # Length of ".html"
     call str_concat
 
 exit_extract_route:
-pop %rbp
-ret
+    pop %rdi                         # Restore the destination buffer from stack
+    pop %r13
+    pop %r12
+    pop %rbp    
+    ret
