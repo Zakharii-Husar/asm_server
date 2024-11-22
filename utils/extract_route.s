939d44b83d4eaed1ce0501a43d5488b395f2c94a
Zakharii-Husar <zakhariihusar@gmail.com>
2024-11-21 19:20:39 -0600

    Restructured extract_route

--- utils/extract_route.s
+++ utils/extract_route.s
@@ -1,10 +1,8 @@
 # Function: extract_route
 # Input: 
-#   %rsi - pointer to the request buffer (global)
-#   %rdi - pointer to the request_route
-# Output: 
-#   %rax - pointer to request_route
-#   %rdx - pointer to request_file_ext
+#   %rdi - pointer to the request_content_buffer
+#   %rsi - pointer to the request_route_buffer
+# Output:  none
 
 .section .data
 .equ SPACE, 32                    # ASCII code for space character
@@ -13,78 +11,61 @@ html_ext: .asciz ".html"          # HTML extension definition moved here
 
 slash_string: .asciz "/" 
 
-.section .bss
-.lcomm request_route, 1024        # Buffer to store the extracted route
-
 .section .text
-# ... existing code ...
 
 .globl extract_route
 .type extract_route, @function
 extract_route:
     push %rbp
     mov %rsp, %rbp
-
-    # Initialize the route register
-    lea request_route(%rip), %rsi     # Destination buffer for route
     
-    # Skip first word (HTTP method) by finding first space
-find_first_space:
-    movb (%rdi), %al
-    cmp $SPACE, %al
-    je found_first_space
-    inc %rdi
-    jmp find_first_space
+    # Find first space using str_find_char
+    mov %rdi, %rsi                   # Move request buffer to %rsi for str_find_char
+    mov $SPACE, %rdi                 # Move space character to %rdi
+    call str_find_char               # Find first space
+    mov %rax, %rdi                   # Move result pointer back to %rdi
+    inc %rdi                         # Skip the space
 
-found_first_space:
-    inc %rdi                          # Skip the space
-
-    # Save start of route
-    mov %rdi, %r9                     # Save route start position
-    
     # Find end of route (next space)
-find_route_end:
-    movb (%rdi), %al
-    cmp $SPACE, %al                   # Check for space
-    je copy_full_route
-    cmp $0, %al                       # Check for null
-    je copy_full_route
-    inc %rdi
-    jmp find_route_end
+    mov %rdi, %r9                    # Save start of route in r9
+    mov %rdi, %rsi                   # Current position to %rsi for str_find_char
+    mov $SPACE, %rdi                 # Space character to %rdi
+    call str_find_char               # Find next space
+    mov %rax, %r8                    # Save end position in r8
 
-copy_full_route:
-    # Calculate route length
-    mov %rdi, %rdx
-    sub %r9, %rdx                     # Length = end - start
-    mov %r9, %rsi                     # Source is route start
-    call str_concat                   # Copy route to request_route
+    # Calculate length and copy route
+    mov %rsi, %rdi                    # Destination is now from parameter (rsi)
+    mov %r9, %rsi                    # Start of route to source
+    mov %r8, %rdx
+    sub %r9, %rdx                    # Calculate length (end - start)
+    call str_concat                  # Copy route to buffer
 
     # Check if route is just "/"
-    lea request_route(%rip), %rsi     # Start of route
+    mov %rsi, %rsi                   # Route buffer already in rsi
     lea slash_string(%rip), %rdi
     call str_cmp
     cmp $0, %rax
-    je search_for_extension           # If not, continue normal flow
-    
+    je search_for_extension          
+
     # Append "index" to the route
-    lea request_route(%rip), %rsi     # Destination buffer
-    lea index_str(%rip), %rdi         # Source (.html extension)
-    xor %rdx, %rdx                    # Let str_concat calculate length
+    mov %rsi, %rsi                   # Route buffer already in rsi
+    lea index_str(%rip), %rdi        
+    xor %rdx, %rdx                   
     call str_concat
 
-search_for_extension:                 # New label for existing extension search
-    lea request_route(%rip), %rsi     # Start of route
+search_for_extension:                
+    mov %rsi, %rsi                   # Route buffer already in rsi
     mov $'.', %rdi
     call str_find_char
-    mov %rax, %r10                    # Save beginning of extension
+    mov %rax, %r10                   
     cmp $1, %rdx
     je exit_extract_route 
 
 no_extension:
-    # Append .html extension since none was found
-    lea request_route(%rip), %rsi     # Destination buffer
-    lea html_ext(%rip), %rdi          # Source (.html extension)
-    xor %rdx, %rdx                    # Let str_concat calculate length
+    # Append .html extension
+    mov %rsi, %rsi                   # Route buffer already in rsi
+    lea html_ext(%rip), %rdi         
+    xor %rdx, %rdx                   
     call str_concat
 
 exit_extract_route:
