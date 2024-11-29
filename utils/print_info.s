cf4b36d36b83603992cdb12b2f5c4c8b01d7ebd2
Zakharii-Husar <zakhariihusar@gmail.com>
2024-11-29 17:40:58 -0600

    Modified print_info to accept parameters in rdi and rsi (according to the convention), and made length(rsi) optional: now you must either provide length or xor rsi before the function call

--- utils/print_info.s
+++ utils/print_info.s
@@ -3,22 +3,30 @@
 .type print_info, @function
 
 # Function: print_info
-# Input: string pointer in %rsi
+# Input: 
+# - %rdi string pointer
+# - %rsi string length (if known or 0 if unknown)
 # Output: none (prints the string to stdout)
 # Note: Internally calculates string length using str_len
 print_info:
     push %rbp
     mov %rsp, %rbp 
-    
-    # Move string pointer to %rdi for str_len
-    mov %rsi, %rdi
-    # preserve string pointer
-    push %rsi
-    call str_len               # str_len will return length in %rax
-    
-    # Set up parameters for write syscall
+
+    mov %rsi, %rdx           # prepare rdx for string length  during  syscall
+    mov %rdi, %rsi           # prepare rsi for string pointer during syscall
+    test %rdx, %rdx
+    jnz .write_to_stdout
+
+    # Calculate string length if unknown
+    push %rsi                  # preserve string pointer
+    call str_len               # %rdi already contains the string pointer, so str_len will return length in %rax
     mov %rax, %rdx             # length returned by str_len
     pop %rsi                   # restore string pointer
+
+    .write_to_stdout:
+    # Set up parameters for write syscall
+    # rdx already contains the string length
+    # rsi already contains the string pointer
     mov $SYS_stdout, %rdi      # file descriptor (stdout)
     mov $SYS_write, %rax       # syscall number for write
     syscall
