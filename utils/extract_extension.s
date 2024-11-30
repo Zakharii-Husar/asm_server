1cace95709bf62cec0918e69885cf1a6de0b0df9
Zakharii-Husar <zakhariihusar@gmail.com>
2024-11-29 21:19:47 -0600

    Remade extract extension

--- utils/extract_extension.s
+++ utils/extract_extension.s
@@ -3,31 +3,46 @@
 
 .globl extract_extension
 .type extract_extension, @function
+
+# Function: extract_extension
+# Parameters:
+#   %rdi - Pointer to the destination buffer where the file extension will be stored
+#   %rsi - Pointer to the route buffer (string) from which the file extension is to be extracted
+
 extract_extension:
     push %rbp
     mov %rsp, %rbp
 
-    # Initialize the route register
-    lea request_route(%rip), %rsi     # Destination buffer for route
+    push %r12
+    mov %rdi, %r12
 
-.copy_extension:
-    # Copy extension (including dot) to extension buffer
-    mov %r10, %rsi                    # %r10 holds beginning of extension
-    lea request_file_ext(%rip), %rdi  # Destination is extension buffer
-    xor %rdx, %rdx                    # Let str_concat calculate length
-    call str_concat
+    # Save %rdi (destination buffer) on the stack
 
-    # Convert route to lowercase
-    lea request_route(%rip), %rsi
-    call str_to_lower
+    # Find the dot in the route buffer
+    mov $'.', %rdx                  # Character to find (dot)
+    call str_find_char              # Call str_find_char with %rsi (route buffer) and %rdx (dot)
+    test %rax, %rax                 # Check if dot was found
+    jz .no_dot_found                # If not found, jump to no dot found
 
-    # Convert extension to lowercase
-    lea request_file_ext(%rip), %rsi
-    call str_to_lower
+    # Copy extension (including dot) to destination buffer
+    mov %r12, %rdi                  # Restore %rdi (destination buffer)
+    mov %rax, %rsi                  # %rax now holds the address of the dot
+    xor %rdx, %rdx                  # Let str_concat calculate length
+    call str_concat                 # Copy from %rsi (dot position) to %rdi (destination buffer)
 
-    # Return pointers to request_route and request_file_ext
-    lea request_route(%rip), %rax      # Return pointer to request_route
-    lea request_file_ext(%rip), %rdx   # Return pointer to request_file_ext
+    # Convert the destination buffer to lowercase
+    mov %r12, %rdi                  # Destination buffer is already in %rdi
+    call str_to_lower               # Convert to lowercase
 
+.no_dot_found:
+
+    # Set default extension ".html"
+    mov %r12, %rdi                  # Destination buffer is already in %rdi
+    mov $".html", %rsi              # Address of default extension string
+    call str_concat                 # Copy default extension to %rdi (destination buffer)
+
+.convert_to_lower:
+
+    pop %r12
     pop %rbp
     ret
