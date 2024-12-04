b470cb989695a9985ecedeca298ad8a989771d94
Zakharii-Husar <zakhariihusar@gmail.com>
2024-12-03 18:23:39 -0600

    Inspected and modified leap year check

--- utils/time/is_leap_year.s
+++ utils/time/is_leap_year.s
@@ -1,6 +1,12 @@
 .section .text
 # Input: rdi = year to check
 # Output: rax = 1 if leap year, 0 if not
+
+# A year is a leap year if:
+# It is divisible by 4, and
+# If divisible by 100, 
+# must also be divisible by 400
+
 .global is_leap_year
 .type is_leap_year, @function
 is_leap_year:
@@ -10,13 +16,12 @@ is_leap_year:
     mov %rdi, %rax
     xor %rdx, %rdx
     mov $4, %rbx
-
     div %rbx
     test %rdx, %rdx
     jnz .not_leap
+
     mov %rdi, %rax
     xor %rdx, %rdx
-
     mov $100, %rbx
     div %rbx
     test %rdx, %rdx
@@ -28,6 +33,7 @@ is_leap_year:
     div %rbx
     test %rdx, %rdx
     jz .leap
+
 .not_leap:
     xor %rax, %rax
     jmp .is_leap_year_end
