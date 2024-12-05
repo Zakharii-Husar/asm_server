431ca24aa848251f3e7a8d2ec00da2f9e04f8453
Zakharii-Husar <zakhariihusar@gmail.com>
2024-12-04 20:34:57 -0600

    Modified str_find_char to return 0 in rax instead of address if we hit the boundary character

--- utils/str_find_char.s
+++ utils/str_find_char.s
@@ -5,7 +5,7 @@
 #   %rdx - Boundary character (0 for unbounded search)
 # Returns:
 #   %rdx - 1 if character found, 0 if not found
-#   %rax - Address of found character or boundary/null
+#   %rax - Address of found character or 0 if not found
 
 str_find_char:
     push %rbp
@@ -17,22 +17,23 @@ str_find_char:
     je .not_found
     test %rdx, %rdx            # Check if boundary char is specified
     jz .skip_boundary_check    # If rdx is 0, skip boundary check
-    cmpb %dl, %al              # Check for boundary character (using dl for byte comparison)
+    cmpb %dl, %al              # Check for boundary character
     je .not_found
 .skip_boundary_check:
-    cmpb %sil, %al             # Compare with search character (using sil for byte comparison)
+    cmpb %sil, %al             # Compare with search character
     je .found_char
     inc %rdi                   # Move to next character
     jmp .search_char
 
 .found_char:
     mov $1, %rdx               # Return 1 if found
-    jmp .finish_str_find_char
+    mov %rdi, %rax            # Return address of character
+    jmp .finish
 
 .not_found:
     mov $0, %rdx               # Return 0 if not found
+    xor %rax, %rax            # Return 0 instead of boundary address
 
-.finish_str_find_char:
-    mov %rdi, %rax            # Return address of character or terminator
+.finish:
     pop %rbp
     ret
