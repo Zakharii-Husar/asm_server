78fed5ae2cd95934b07ec4b4a0d73d8af692aafb
Zakharii-Husar <zakhariihusar@gmail.com>
2024-12-12 16:56:33 -0600

    rewrote file_open added destination buffer size as another parameter added buffer clearance changed max buffer size read (which possibly was causing a bug buffer overflow before adding null termination added check that we have space for the /bin/bash

--- utils/file_open.s
+++ utils/file_open.s
@@ -1,6 +1,3 @@
-.section .bss
-.lcomm stat_buffer, 100
-
 .section .data
 
 file_open_err_msg:    .asciz "\033[31mFailed to open file! ❌\033[0m\n"
@@ -15,6 +12,13 @@ close_err_msg_length = . - close_err_msg
 read_err_msg:    .asciz "\033[31mFailed to read file! ❌\033[0m\n"
 read_err_msg_length = . - read_err_msg
 
+line_break: .asciz "\n"
+
+stat_buffer_size = 100
+
+.section .bss
+.lcomm stat_buffer, stat_buffer_size
+
 .section .text
 
 .type file_open, @function
@@ -23,7 +27,8 @@ file_open:
     # Parameters:
     # %rdi contains the file path to open
     # %rsi contains the buffer to write into
-    # %rdx contains null termination flag (1 = terminate, 0 = don't terminate)
+    # %rdx contains the buffer size
+    # %rcx contains null termination flag (1 = terminate, 0 = don't terminate)
 
     # Return value:
     # %rax on success returns file size
@@ -39,61 +44,82 @@ file_open:
     # lea file_buffer(%rip), %rsi
     # call file_open
 
+
+    # 1. SAVE REGISTERS
     push %rbp
     mov %rsp, %rbp
+    push %rbx
     push %r12
+    push %r13
+    push %r14
 
-    mov %rsi, %r8      # response buffer
+    # 2. SAVE PARAMETERS
+    mov %rsi, %rbx      # response buffer
     mov $0, %r9        # Initialize return file size
-    mov %rdx, %r12     # save null termination flag
+    mov %rcx, %r12     # save null termination flag
+    mov %rdi, %r13     # save file path
+    mov %rdx, %r14     # save buffer size
+
+    # 3. CLEAR THE BUFFERS
+    lea stat_buffer(%rip), %rdi
+    mov $stat_buffer_size, %rsi
+    call clear_buffer
+
+    mov %rbx, %rdi     # buffer address
+    mov %r14, %rsi     # buffer size
+    call clear_buffer
     
-    # Open the file using the path in %rdi
-    # mov %rdi, %rdi already contains file path from caller
+    # 4. OPEN THE FILE
+    mov %r13, %rdi                     # file path
     mov $SYS_open, %rax                # sys_open
     mov $0, %rsi                       # flags = O_RDONLY
     syscall                            # path is already in %rdi
 
-    mov %rax, %r10 # file descriptor
+    mov %rax, %r10                     # file descriptor
 
     # If negative, it's an error
     cmp $0, %rax
     jl .handle_file_open_error        # jump to error handling if failed to open
 
 
-    # Read file contents into the buffer passed in %rdi
-    mov $SYS_read, %rax              # sys_read
-    mov %r8, %rsi                    # Use the buffer pointer passed in %rsi > %r9
+    # 5. READ FILE CONTENTS INTO THE BUFFER
+    mov $SYS_read, %rax               # sys_read
+    mov %rbx, %rsi                    # Use the buffer pointer passed in %rsi > %rbx
     mov %r10, %rdi                    # file descriptor
-    mov $response_content_B_size, %rdx                  # max bytes to read
+    mov %r14, %rdx                    # max bytes to read
     syscall
 
     cmp $0, %rax
     jl .handle_read_error   
 
-    # Save bytes read temporarily
+    # 6. SAVE BYTES READ TEMPORARILY
     mov %rax, %r9
 
-    # Check if null termination was requested
+    # 7. CHECK IF NULL TERMINATION WAS REQUESTED
     cmp $1, %r12
     jne .skip_termination
     
-    # Add null terminator after the last byte read
-    mov %r8, %rdi
+    # 7.1. ENSURE WE HAVE SPACE FOR NULL TERMINATOR
+    cmp %r14, %r9              # Compare bytes_read with buffer_size
+    jge .handle_buffer_overflow  # If bytes_read >= buffer_size, we can't add null terminator
+
+    # 7.2. ADD NULL TERMINATOR AFTER THE LAST BYTE READ
+    mov %rbx, %rdi
     add %r9, %rdi      # Point to byte after last read byte
     movb $0, (%rdi)    # Add null terminator
     
 .skip_termination:
-    # Get file size using fstat
+    # 8. GET FILE SIZE USING FSTAT
     mov $SYS_fstat, %rax             # sys_fstat
     mov %r10, %rdi                    # file descriptor
     lea stat_buffer(%rip), %rsi      # address of struct stat
     syscall
 
-    # Check if fstat was successful
+    # 8.1. CHECK IF FSTAT WAS SUCCESSFUL
     cmp $0, %rax
     jl .handle_fstat_error
 
-    # Get the file size from stat_buffer
+    # 8.2. GET THE FILE SIZE FROM STAT_BUFFER
     # The reason I need to dereference pointer and store the file size
     # here: even tho subsequent syscall doesn't use rsi,
     # content switches from user mode to kernel mode and overwrites rsi
@@ -102,7 +128,7 @@ file_open:
     mov 48(%rsi), %rsi  # st_size is usually at offset 40 for 64-bit
     mov %rsi, %r9 # return file size
 
-    # Close the file descriptor
+    # 9. CLOSE THE FILE DESCRIPTOR
     mov $SYS_close, %rax            # sys_close
     mov %r10, %rdi                   # file descriptor
     syscall
@@ -111,14 +137,14 @@ file_open:
     jl .handle_close_error
     
     mov %r9, %rax # return file size
-    # Check if we need to include null terminator in returned size
+    # 9.1. CHECK IF WE NEED TO INCLUDE NULL TERMINATOR IN RETURNED SIZE & RETURN
     cmp $1, %r12
     jne .exit_file_open
     inc %rax                        # Include null terminator in size if requested
     jmp .exit_file_open             # jump to error handling if failed to open
 
 
-
+   # 10. HANDLE ERRORS
     .handle_file_open_error:
         
         lea file_open_err_msg(%rip), %rdi   
@@ -129,6 +155,11 @@ file_open:
         jmp .exit_file_open
 
     .handle_read_error:
+        # Close file
+        mov $SYS_close, %rax
+        mov %r10, %rdi
+        syscall
+        # Continue with error handling
         lea read_err_msg(%rip), %rdi
         mov $read_err_msg_length, %rsi
         call print_info
@@ -152,7 +183,16 @@ file_open:
         mov $-1, %rax
             
 
+    .handle_buffer_overflow:
+        # Handle like other errors
+        mov $-1, %rax
+        jmp .exit_file_open
+
     .exit_file_open:
+        # Restore registers
+        pop %r14
+        pop %r13
         pop %r12
+        pop %rbx
         pop %rbp
         ret
