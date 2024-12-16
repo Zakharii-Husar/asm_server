89901a9ac54de12de1050b7f4e6fb5e4af5a0148
Zakharii-Husar <zakhariihusar@gmail.com>
2024-12-16 17:58:19 -0600

    Modified adjust_timezone to expect hours and minutes in format '-0600' or '600'

--- utils/time/adjust_timezone.s
+++ utils/time/adjust_timezone.s
@@ -6,19 +6,34 @@ adjust_timezone:
     push %rbp
     mov %rsp, %rbp
     push %r12
+    push %r13
 
     mov %rdi, %r12              # Store UTC timestamp
     
-    # Prepare to call str_to_int
+    # Get timezone offset
     mov CONF_TIMEZONE_OFFSET(%r15), %rax
+    
+    # Split into hours and minutes
+    mov %rax, %r13              # Save original offset
+    mov $100, %rcx
+    cqo                         # Sign-extend RAX into RDX
+    idiv %rcx                   # Divide by 100
+                               # Now: RAX = hours, RDX = minutes
     
     # Convert hours to seconds (multiply by 3600)
     imul $3600, %rax, %rax
     
-    # Simply add the offset (will subtract if negative)
+    # Convert minutes to seconds (multiply by 60)
+    imul $60, %rdx, %rdx
+    
+    # Add hours and minutes
+    add %rdx, %rax
+    
+    # Add the total offset to timestamp
     add %rax, %r12
     mov %r12, %rax              # Put result in return register
 
+    pop %r13
     pop %r12
     pop %rbp
     ret
