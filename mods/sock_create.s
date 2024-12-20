3e76055a2146de56e8e86c0a0c9cb14ddb00a728
Zakharii-Husar <zakhariihusar@gmail.com>
2024-12-19 20:45:24 -0600

    Added error handling to socket_create

--- mods/sock_create.s
+++ mods/sock_create.s
@@ -1,7 +1,6 @@
 .section .rodata
 
-.sock_created_msg:       .asciz "\033[32mTCP Sock was created ✅\033[0m\n"
-.sock_create_err_msg:    .asciz "\033[31mFailed to create TCP socket ❌\033[0m\n"
+.sock_create_err_msg:    .asciz "CRITICAL: Failed to create TCP socket in sock_create.s"
 .sock_create_err_msg_length = . - .sock_create_err_msg  
 
 .section .text
@@ -9,8 +8,9 @@
 # Parameters:
 #   - None (uses predefined constants for socket creation)
 # Return Values:
-#   - Returns a socket file descriptor (fd) on success (fd >= 0)
-#   - Calls exit_program on failure (fd < 0)
+#   - none
+# Side Effects:
+#   - %r12: Socket file descriptor (fd)
 
 .type sock_create, @function
 
@@ -29,15 +29,17 @@ sock_create:
  jl  .handle_sock_create_err                 # Jump to error handling if %rax < 0
  mov %rax, %r12                              # save socket fd to %r12
 
- # lea .sock_created_msg(%rip), %rsi
- # call print_info
-
  pop %rbp                              # restore the caller's base pointer
  ret                                   # return to the caller
 
-.handle_sock_create_err:
- lea .sock_create_err_msg(%rip), %rdi
- mov $.sock_create_err_msg_length, %rsi
- call print_info
- call exit_program
 
+.exit_sock_create:
+ pop %rbp
+ ret
+
+ .handle_sock_create_err:
+ lea sock_create_err_msg(%rip), %rdi
+ mov $sock_create_err_msg_length, %rsi
+ call log_err
+ mov $-1, %rax
+ jmp .exit_sock_create
