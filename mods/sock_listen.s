96f25ed8631e43be94a38fd249e5ff48d26efb13
Zakharii-Husar <zakhariihusar@gmail.com>
2024-12-19 20:48:01 -0600

    Added error handling to sock_listen

--- mods/sock_listen.s
+++ mods/sock_listen.s
@@ -1,20 +1,19 @@
 .section .rodata
 
-.sock_listen_msg:    .asciz "\033[32mTCP Sock is listening 🎧\033[0m\n"
-.sock_listen_msg_length = . - .sock_listen_msg
-
-.sock_listen_err_msg:    .asciz "\033[31mSocket failed to listen ❌\033[0m\n"
+.sock_listen_err_msg:    .asciz "CRITICAL: Socket failed to listen in sock_listen.s"
 .sock_listen_err_msg_length = . - .sock_listen_err_msg
 
 .section .text
 
 # Function: sock_listen
 # Parameters:
-#   - %rdi: socket file descriptor (fd) to listen on
-#   - %rsi: backlog size for the listen queue
+#   - none
+# Implicit Parameters:
+#   - %r12: Socket file descriptor (fd)
 # Return Values:
-#   - Returns 0 on success (socket is now listening)
-#   - Calls exit_program on failure (if listen fails)
+#   - none
+# Side Effects:
+#   - listen() syscall
 
 .type sock_listen, @function
 sock_listen:
@@ -28,17 +27,16 @@ sock_listen:
 
  cmp $0, %rax                               # Compare the return value with 0
  jl  .handle_sock_listen_err                # Jump to error handling if %rax < 0
-    
- # lea .sock_listen_msg(%rip), %rsi           # pointer to the message (from constants.s)
- # mov $.sock_listen_msg_length, %rdx         # length of the message (from constants.s)
- # call print_info
 
+
+.exit_sock_listen:
  pop %rbp                     # restore the caller's base pointer
  ret                           # return to the caller
 
 .handle_sock_listen_err:
- lea .sock_listen_err_msg(%rip), %rdi           # pointer to the message (from constants.s)
- mov $.sock_listen_err_msg_length, %rsi        # length of the message (from constants.s)
- call print_info
- call exit_program
+ lea sock_listen_err_msg(%rip), %rdi
+ mov $sock_listen_err_msg_length, %rsi
+ call log_err
+ mov $-1, %rax
+ jmp .exit_sock_listen
  
