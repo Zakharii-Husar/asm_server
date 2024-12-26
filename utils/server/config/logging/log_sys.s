8fcbfc9053b30aa162218c33d0a22b0c9623fa61
Zakharii-Husar <zakhariihusar@gmail.com>
2024-12-26 14:33:09 -0600

    Renamed socket logs to system logs

--- utils/server/config/logging/log_sys.s
+++ utils/server/config/logging/log_sys.s
@@ -1,15 +1,15 @@
 .section .rodata
-socket_prefix_base: .asciz " SOCKET"
-socket_prefix_base_length = . - socket_prefix_base
+sys_prefix_base: .asciz " SYSTEM"
+sys_prefix_base_length = . - sys_prefix_base
 
-.equ sock_log_B_size, 1024
+.equ sys_log_B_size, 1024
 
 .section .bss
-.lcomm sock_log_B, sock_log_B_size   # Buffer for constructing socket log entry
+.lcomm sys_log_B, sys_log_B_size   # Buffer for constructing socket log entry
 
 .section .text
-.globl log_sock
-log_sock:
+.globl log_sys
+log_sys:
     # Parameters:
     # rdi - pointer to socket event description string
     # rsi - string length
@@ -24,52 +24,52 @@ log_sock:
     mov %rsi, %r13   # Save length
     
     # Start with empty buffer
-    lea sock_log_B(%rip), %rdi
-    mov $sock_log_B_size, %rsi
+    lea sys_log_B(%rip), %rdi
+    mov $sys_log_B_size, %rsi
     call clear_buffer
     
     # Get timestamp and add it
     call get_time_now
     mov %rax, %rsi
     xor %rdx, %rdx
-    lea sock_log_B(%rip), %rdi
-    mov $sock_log_B_size, %rcx
+    lea sys_log_B(%rip), %rdi
+    mov $sys_log_B_size, %rcx
     call str_cat
 
     # Add socket prefix
-    lea sock_log_B(%rip), %rdi
+    lea sys_log_B(%rip), %rdi
     lea socket_prefix_base(%rip), %rsi
     mov $socket_prefix_base_length, %rdx
-    mov $sock_log_B_size, %rcx
+    mov $sys_log_B_size, %rcx
     call str_cat
     
     # Add semicolon separator
-    lea sock_log_B(%rip), %rdi
+    lea sys_log_B(%rip), %rdi
     lea semicolon_char(%rip), %rsi
     mov $1, %rdx
-    mov $sock_log_B_size, %rcx
+    mov $sys_log_B_size, %rcx
     call str_cat
 
     # Add event description
-    lea sock_log_B(%rip), %rdi
+    lea sys_log_B(%rip), %rdi
     mov %r12, %rsi        # Description string
     mov %r13, %rdx        # Length
-    mov $sock_log_B_size, %rcx
+    mov $sys_log_B_size, %rcx
     call str_cat
 
     # Add newline
-    lea sock_log_B(%rip), %rdi
+    lea sys_log_B(%rip), %rdi
     lea newline_char(%rip), %rsi
     mov $1, %rdx
-    mov $sock_log_B_size, %rcx
+    mov $sys_log_B_size, %rcx
     call str_cat
     
     # Write to access log file
-    lea sock_log_B(%rip), %rdi
+    lea sys_log_B(%rip), %rdi
     call str_len
     mov %rax, %rdx
-    lea sock_log_B(%rip), %rsi
-    mov CONF_ACCESS_LOG_FD_OFFSET(%r15), %rdi
+    lea sys_log_B(%rip), %rsi
+    mov CONF_SYSTEM_LOG_FD_OFFSET(%r15), %rdi
     mov $SYS_write, %rax
     syscall
 
